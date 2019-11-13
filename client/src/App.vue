535b5ee534deb8ad782cee4b2041fd86488b5902
Sergey Ustinov <ustinov.biz@gmail.com>
2019-11-13 12:15:46 +0300

    Add Header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <app-header></app-header>
+  </div>
 </template>
+
+<script>
+import Header from './components/Header/Header.vue';
+export default {
+  components: {
+    appHeader: Header
+  }
+};
+</script>
 
 <style></style>
---
+++ client/src/components/Header/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container class="grey cont elevation-5">
+    <v-row class="header elevation-15 justify-center">
+      <v-col cols="2">
+        <img src="../../assets/logo.svg" alt="Tom Yum" class="logo" />
+      </v-col>
+      <v-col cols="2">
+        <app-header-auth></app-header-auth>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import HeaderAuth from './HeaderAuth.vue';
+export default {
+  data() {
+    return {
+      username: 'Username'
+    };
+  },
+  components: {
+    appHeaderAuth: HeaderAuth
+  }
+};
+</script>
+
+<style>
+.cont {
+  background-color: grey;
+  margin: 0;
+  padding: 0;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+}
+
+.logo {
+  width: 64px;
+  height: 64px;
+}
+</style>
